c2d7b714d493da741bec1a9259f5fac37f469e4e
Adit P Patel <app2116@gmail.com>
2020-01-25 15:53:32 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -16,7 +16,6 @@ def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
   pp input
-  pp (Integer(input))
   begin
   legal_integer = ((songs.length+1) >= Integer(input))
     rescue 
